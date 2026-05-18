c3ca7accae8e70061d2252f28c0d32e9f3f7a523
vedika-lende <vedlende@gmail.com>
2026-05-18 08:08:32 -0700

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Python -Based Calculator
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error! Division by zero is not allowed."
+    else:    
+        return a / b
+
+
+while True:
+    print("\n===== Python Calculator =====")
+    print("1.Addition")
+    print("2.Subtraction")
+    print("3.Multiplication")
+    print("4.Division")
+    print("5.Exit")
+
+    choice = input("Enter your choice(1-5):")
+
+    if choice == '5':
+        print("Calculator Closed")
+        break
+
+    if choice in ['1', '2', '3','4']:
+
+        try:
+            num1=float(input("Enter first number:"))
+            num2=float(input("Enter second number:"))
+
+            if choice == '1':
+                print("Result:", add(num1 ,num2))
+
+            elif choice == '2':
+                print("Result:", subtract(num1, num2))
+
+            elif choice == '3':
+                print("Result:", multiply(num1, num2))
+
+            elif choice == '4':
+                print("Result:", divide(num1, num2))
+
+        except ValueError :
+            print("Invalid input! Please enter numeric values.")
+    else:
+
+       print("Invalid choice! Please select between 1 and 5.")
+
+
+
+# In[ ]:
+
+
+
+
